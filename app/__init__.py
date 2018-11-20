bc8d0aae74791738f9926bd315fd0b143fafc8cf
munniomer <munniomer@gmail.com>
2018-11-20 12:59:27 +0300

    [finishes #162038007] adds the initialize_db method in the create app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, Blueprint
 from config import app_config
+from .database.db_connection import initialize_db
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
+    initialize_db()
     return app
