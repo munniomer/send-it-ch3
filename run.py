0f52efc0bf568723e210c17b1575fdfd1cac8d82
munniomer <munniomer@gmail.com>
2018-11-22 23:31:39 +0300

    removes an identation

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app.database.db_connection import create_tables, destroy_tables
 config_name = os.getenv("FLASK_ENV")
 app = create_app(config_name)
 
-    create_tables()
+create_tables()
 
 if __name__ == "__main__":
     app.run(debug=True)
