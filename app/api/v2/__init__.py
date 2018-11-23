3ca1557099cb0188b4a6f9b036f1d541d7eb4ed5
munniomer <munniomer@gmail.com>
2018-11-23 03:27:00 +0300

    registers the login endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource 
-from app.api.v2.views.users_view import SignupResource
+from app.api.v2.views.users_view import SignupResource,LoginResource
 
 v2 = Blueprint('apiv1', __name__, url_prefix='/api/v2')
 app = Api(v2)  
 
 # Users
 app.add_resource(SignupResource, '/auth/signup')
+app.add_resource(LoginResource, '/auth/login')
